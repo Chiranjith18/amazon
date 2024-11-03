2d9ad662c7cb1e316efc4c3842e8ab06c2aac4d8
Chiranjith18 <145703565+Chiranjith18@users.noreply.github.com>
2024-11-03 23:11:28 +0530

    Update Dolphin.java

--- Dolphin.java
+++ Dolphin.java
@@ -1,17 +1,29 @@
 public class Dolphin extends Animal implements Swim {
     private int swimmingSpeed; // Speed in nautical miles per hour
+    private String color; // Color of the dolphin
 
     public Dolphin() {
         super("Dolphin"); // Call the parent constructor with the dolphin's name
     }
 
-    // Add the following methods exactly as needed
+    // Method to set the swimming speed
     public void setSwimmingSpeed(int swimmingSpeed) {
         this.swimmingSpeed = swimmingSpeed; // Set the swimming speed
     }
 
+    // Method to get the swimming speed
     public int getSwimmingSpeed() {
         return swimmingSpeed; // Return the current swimming speed
+    }
+
+    // Method to set the color
+    public void setColor(String color) {
+        this.color = color; // Set the color of the dolphin
+    }
+
+    // Method to get the color
+    public String getColor() {
+        return color; // Return the current color of the dolphin
     }
 
     @Override
